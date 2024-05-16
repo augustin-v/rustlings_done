297d199be035f206131b323baa8628112a9e163b
Augustin <avilletardpiano@gmail.com>
2024-05-16 09:28:57 -0400

    aa

---
+++ exercises/clippy/target/debug/deps/clippy3-ffdfc88001c1f845.d
@@ -0,0 +1,9 @@
+/home/kali/rustlings/exercises/clippy/target/debug/deps/libclippy3-ffdfc88001c1f845.rmeta: clippy3.rs Cargo.toml
+
+/home/kali/rustlings/exercises/clippy/target/debug/deps/clippy3-ffdfc88001c1f845.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
